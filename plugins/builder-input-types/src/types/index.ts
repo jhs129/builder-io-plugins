2b177f72b1d951de096689164d67a1edfd93dcd0
jhs129 <jhs129@users.noreply.github.com>
2025-07-27 13:24:18 -0400

    checkpoint

--- plugins/builder-input-types/src/types/index.ts
+++ plugins/builder-input-types/src/types/index.ts
@@ -1,17 +1,2 @@
-export interface ApiTagsType {
-  name: string;
-  endPoint: string;
-}
-
-export interface ApiTagsSettings {
-  types: ApiTagsType[];
-}
-
-export interface ApiTagOption {
-  value: string;
-  label?: string;
-}
-
-export interface ApiTagsPluginContext {
-  iframeRef?: { current: HTMLIFrameElement | null };
-}
+export * from './NumberSliderTypes';
+export * from './CMSLinkTypes';
